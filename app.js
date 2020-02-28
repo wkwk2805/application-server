855ae607531d2d626011a30cb3962455cf27b243
unknown <wkwk2805@gmail.com>
2020-02-28 15:59:00 +0900

    define certification

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ const handleError = err => console.log(`❌ Error on DB Connection : ${err}`);
 db.once("open", handleOpen);
 db.on("error", handleError);
 
+const resultObj = (isSuccess, message, returnValue) => {
+  return {
+    success: isSuccess,
+    message: message,
+    returnValue: returnValue
+  };
+};
+
 const userSchema = mongoose.Schema({
   user_id: String,
   user_pw: String,
@@ -27,19 +35,20 @@ const User = mongoose.model("user", userSchema);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
+app.post("/insert", (req, res) => {
   console.log("request contents", req.body);
   const user = new User({ user_id: req.body.id, user_pw: req.body.pw });
   user.save((err, data) => {
     if (err) throw err;
-    console.log(data);
-    let isSuccess = false;
-    let message = "message";
-    const result = {
-      success: isSuccess,
-      message: message
-    };
-    res.json(result);
+    res.json(resultObj(true, "등록성공", data));
+  });
+});
+
+app.post("/certification", (req, res) => {
+  console.log("/certification", req.body);
+  User.find({ user_id: req.body.id, user_pw: req.body.pw }, (err, data) => {
+    if (err) throw err;
+    res.json(resultObj(true, "인증성공", data));
   });
 });
 
