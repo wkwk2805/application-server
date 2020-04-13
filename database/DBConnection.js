a2624a9bc3ddd4768249b016a4c0be6b70a77102
unknown <wkwk2805@gmail.com>
2020-04-13 18:03:23 +0900

    db setting

--- database/DBConnection.js
+++ database/DBConnection.js
@@ -1,21 +1,17 @@
 const mongoose = require("mongoose");
 
 const DBConnection = () => {
-  mongoose.connect(
-    "mongodb://localhost:27017,localhost:27018,localhost:27019/faith_book",
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      replicaSet: "rs0"
-    }
-  );
+  mongoose.connect("mongodb://localhost:27017/faith_book", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 
   const db = mongoose.connection;
 
   const handleOpen = () => console.log("connected to DB");
-  const handleError = err => console.log(`Error on DB Connection : ${err}`);
+  const handleError = (err) => console.log(`Error on DB Connection : ${err}`);
 
   db.once("open", handleOpen);
   db.on("error", handleError);
